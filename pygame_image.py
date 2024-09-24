1c121fa6dfd85828da32dcfa1befaf3971d801ea
SorimachiRikuto <c0a2309147@edu.teu.ac.jp>
2024-09-24 17:11:05 +0900

    練習２：こうかとんの読み込み

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,8 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    kk_img = pg.image.load("fig/3.png")  # 練習２
+    kk_img = pg.transform.flip(kk_img, True, False)  # 練習２
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -25,4 +27,3 @@ if __name__ == "__main__":
     pg.init()
     main()
     pg.quit()
-    sys.exit()
