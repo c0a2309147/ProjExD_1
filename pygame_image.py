eeee02cd63a845f9de291fb0bf4d1f5875c39255
SorimachiRikuto <c0a2309147@edu.teu.ac.jp>
2024-09-24 17:41:58 +0900

    演習2 完了

--- pygame_image.py
+++ pygame_image.py
@@ -16,30 +16,44 @@ def main():
     kk_img = pg.image.load("fig/3.png")  # 練習２
     kk_img = pg.transform.flip(kk_img, True, False)  # 練習２
     kk_rct = kk_img.get_rect()  # 練習8-1：SurfaceからRectを抽出する
-    kk_rct.center = 300, 200  # 練習8-2：Rectを使った初期座標の設定
+    kk_rct.center = 300, 200  # 練習8-2：Rectを使った初期座標の設定 
     
     tmr = 0
     bg_speed = 1  # 背景の初期速度
 
     while True:
+    
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
 
         key_lst = pg.key.get_pressed()  # 練習8-3：キーの押下状態を取得
+        move_x = 0 # 移動処理を行う座標
+        move_y = 0
 
-        # こうかとんの移動処理
-        if key_lst[pg.K_UP]:  # 上矢印キーがTrueなら
-            kk_rct.move_ip(0, -1)  # こうかとんの縦座標を-1する
-        if key_lst[pg.K_DOWN]:  # 下矢印キーがTrueなら
-            kk_rct.move_ip(0, +1)  # こうかとんの縦座標を+1する
-        if key_lst[pg.K_LEFT]:  # 左矢印キーがTrueなら
-            kk_rct.move_ip(-1, 0)  # こうかとんの横座標を-1する
-        if key_lst[pg.K_RIGHT]:  # 右矢印キーがTrueなら
-            kk_rct.move_ip(+2, 0)  # こうかとんの横座標を+1する
+        # # こうかとんの移動処理
+        # if key_lst[pg.K_UP]:  # 上矢印キーがTrueなら
+        #     kk_rct.move_ip(0, -1)  # こうかとんの縦座標を-1する
+        # if key_lst[pg.K_DOWN]:  # 下矢印キーがTrueなら
+        #     kk_rct.move_ip(0, +1)  # こうかとんの縦座標を+1する
+        # if key_lst[pg.K_LEFT]:  # 左矢印キーがTrueなら
+        #     kk_rct.move_ip(-1, 0)  # こうかとんの横座標を-1する
+        # if key_lst[pg.K_RIGHT]:  # 右矢印キーがTrueなら
+        #     kk_rct.move_ip(+2, 0)  # こうかとんの横座標を+1する
+        # else:
+        #     kk_rct.move_ip(-1, 0)  # それ以外は通常速度
 
-        else:
-            kk_rct.move_ip(-1, 0)  # それ以外は通常速度
+        if key_lst[pg.K_UP]:
+            move_y = -1
+        if key_lst[pg.K_DOWN]:
+            move_y = 1
+        if key_lst[pg.K_LEFT]:
+            move_x = -1
+        if key_lst[pg.K_RIGHT]:
+            move_x = 2
+
+        kk_rct.move_ip(move_x, move_y)
+
 
         # 背景の移動処理
         x = -(tmr * bg_speed % 3200)  # 倍速対応で背景を左に流す
